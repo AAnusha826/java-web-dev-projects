37ce50f0da216b1b99b9133b14980ff7b5912005
AAnusha26 <adabalaanusha26@gmail.com>
2024-09-05 21:13:14 -0400

    Update Circle and Area classes with latest changes

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Circle.java
@@ -0,0 +1,8 @@
+package org.launchcode;
+
+public class Circle {
+    // Method to calculate the area of a circle
+    public static double getArea(double radius) {
+        return Math.PI * radius * radius; // Formula: π * r^2
+    }
+}
